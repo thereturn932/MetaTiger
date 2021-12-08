be06cfe9f189c317c5666d5cc64523e5a6523a91
thereturn932 <mburakozenn@gmail.com>
2021-12-08 22:34:31 +0300

    Clean up

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MetaTiger = await hre.ethers.getContractFactory("MetaTiger");
+  const metaTiger = await MetaTiger.deploy();
 
-  await greeter.deployed();
+  await metaTiger.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Meta Tiger deployed to:", metaTiger.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
